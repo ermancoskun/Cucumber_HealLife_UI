a08c1111893eb78bb5eb334df1f3d3494bc09b1f
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-05-11 11:08:17 +0300

    11.5.23 ayazicioglu

--- src/test/java/stepdefinitions/AdminStepdefinitions.java
+++ src/test/java/stepdefinitions/AdminStepdefinitions.java
@@ -1,4 +1,29 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import utilities.Driver;
+import utilities.HealMethods;
+
 public class AdminStepdefinitions {//
+
+    @Given("Goes to Admin login page")
+    public void goes_to_admin_login_page() {
+        // intentionally left blank
+    }
+    @Then("Login with {string} and {string} datas")
+    public void login_with_and_datas(String username, String password) {
+        HealMethods.loginAsAdmin(username,password);
+    }
+    @Then("Click on the {string} link")
+    public void clickOnTheLink(String link) {
+        HealMethods.clickASidebarLink(link);
+    }
+    @Then("Verified redirected to Billing page")
+    public void verified_redirected_to_billing_page() {
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("bill"));
+    }
+
+
 }

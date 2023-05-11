a7d133cda4828e062a30da99a1f71e1aa709c71a
duygugunaydin <dyg.gunaydin@gmail.com>
2023-05-11 23:17:29 +0300

    duygu/11/05/v1

--- src/test/java/stepdefinitions/AdminStepdefinitions.java
+++ src/test/java/stepdefinitions/AdminStepdefinitions.java
@@ -1,12 +1,23 @@
 package stepdefinitions;
 
+import com.github.javafaker.Faker;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
+import pages.AdminPage;
 import utilities.Driver;
 import utilities.HealMethods;
 
-public class AdminStepdefinitions {//
+
+
+
+
+
+public class AdminStepdefinitions {
+    AdminPage adminPage=new AdminPage();
+    Faker faker=new Faker();
+    AdminPage adminpage=new AdminPage();
 
     @Given("Goes to Admin login page")
     public void goes_to_admin_login_page() {
@@ -24,6 +35,102 @@ public class AdminStepdefinitions {//
     public void verified_redirected_to_billing_page() {
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("bill"));
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////DGup
+    @And("Login to admin page as {string} {string}")
+    public void loginToAdminPageAs(String username, String password) {
+        adminpage.loginAsAdmin(username,password);
+    }
+
+    @And("Verify that the heallife logo is visible in the navbar of the admin panel.")
+    public void verifyThatTheHeallifeLogoIsVisibleInTheNavbarOfTheAdminPanel() {
+        Assert.assertTrue("Heallife logo is not visible in the navbar of the admin panel",adminpage.heallifeLogo.isDisplayed());
+    }
+
+    @And("Click on the Billing Menu")
+    public void clickOnTheBillingMenu() {
+        adminpage.billingMenu.click();
+    }
+
+    @And("Click on the heallife logo in the Navbar")
+    public void clickOnTheHeallifeLogoInTheNavbar() {
+        adminpage.heallifeLogo.click();
+    }
+
+    @And("Verify that the admin panel is refreshed when clicking on the heallife logo")
+    public void verifyThatTheAdminPanelIsRefreshedWhenClickingOnTheHeallifeLogo() {
+        Assert.assertTrue("Admin panel does not refresh when clicking on heallife logo",adminpage.welcomePartyAnnouncement.isEnabled());
+        Assert.assertTrue("Admin panel does not refresh when clicking on heallife logo",adminpage.welcomePartyAnnouncement.isDisplayed());
+    }
+
+    @And("Verify that there is a dashboard menu icon in the navbar")
+    public void verify_that_there_is_a_dashboard_menu_icon_in_the_navbar() {
+        Assert.assertTrue("No dashboard menu icon in the navigation bar",adminpage.dashboardMenuIcon.isDisplayed());
+        Assert.assertTrue("A dashboard menu icon is not clickable in the navigation bar",adminpage.dashboardMenuIcon.isEnabled());
+    }
+
+    @And("Click on the menu icon.")
+    public void click_on_the_menu_icon() {
+        adminpage.dashboardMenuIcon.click();
+    }
+
+    @And("Verify that the dashboard menu is closed when clicked")
+    public void verify_that_the_dashboard_menu_is_closed_when_clicked() {
+        Assert.assertFalse("Dashboard menu does not open and close when clicked",adminpage.billingMenu.isDisplayed());
+    }
+
+    @And("Verify that the dashboard menu is opened when clicked")
+    public void verify_that_the_dashboard_menu_is_opened_when_clicked() {
+        Assert.assertTrue("Dashboard menu does not open and close when clicked",adminpage.billingMenu.isDisplayed());
+    }
+
+    @And("Verify that the Navbar has the text *Heal Life Hospital & Research Center*")
+    public void verifyThatTheNavbarHasTheTextHealLifeHospitalResearchCenter() {
+        Assert.assertTrue("The text *Heal Life Hospital & Research Center* is not visible in Navbar",adminpage.healLifeHospitalResearchCenterText.isDisplayed());
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////DGdown
+
+
+
 
 
 }
--- src/test/java/stepdefinitions/HomepageStepdefinitions.java
+++ src/test/java/stepdefinitions/HomepageStepdefinitions.java
@@ -1,4 +1,59 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import pages.HomePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+
 public class HomepageStepdefinitions {//
+
+    Actions actions = new Actions(Driver.getDriver());
+    HomePage homepage= new HomePage();
+
+    /////////////////////////////////////////////////////////////////////////////////////
+    @Given("Launch browser.")
+    public void launchBrowser() {
+        Driver.getDriver();}
+
+    @Then("Go to {string}")
+    public void goTo(String url) {
+        Driver.getDriver().get(ConfigReader.getProperty(url));}
+
+    @And("Go to the home page footer section.")
+    public void goToTheHomePageFooterSection() {actions.sendKeys(Keys.END).perform();}
+
+    @And("Verify that the complain button and icon are visible.")
+    public void verifyThatTheComplainButtonAndIconAreVisible() {
+        Assert.assertTrue("Complain icon are not visible",homepage.complainIcon.isDisplayed());
+        Assert.assertTrue("Complain button are not visible",homepage.complainButton.isDisplayed());
+        Assert.assertTrue("Complain icon are not clickable",homepage.complainIcon.isEnabled());
+        Assert.assertTrue("Complain button are not clickable",homepage.complainButton.isEnabled());}
+
+    @And("Close browser")
+    public void closeBrowser() {
+        Driver.closeDriver();}
+
+    @And("Click on the complainIcon")
+    public void clickOnTheComplainIcon() {
+        homepage.complainButton.click();}
+
+    @And("Verify that it redirects to the {string} page.")
+    public void verifyThatItRedirectsToThePage(String url) {
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+        String expectedUrl = ConfigReader.getProperty(url);
+
+        Assert.assertEquals("The 'Complain' button on the homepage does not redirect to the complain page.",expectedUrl,actualUrl);}
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 }

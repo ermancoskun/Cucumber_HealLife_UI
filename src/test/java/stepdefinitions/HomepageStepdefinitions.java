2b363ffee9e66d6c829b0f653ed43c82dc5612b6
saidozkan2020 <said_ozkan@hotmail.com>
2023-05-11 18:31:34 +0200

    11.05.23 Said

--- src/test/java/stepdefinitions/HomepageStepdefinitions.java
+++ src/test/java/stepdefinitions/HomepageStepdefinitions.java
@@ -1,4 +1,43 @@
 package stepdefinitions;
 
-public class HomepageStepdefinitions {//
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import pages.HomePage;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+import java.util.List;
+
+
+public class HomepageStepdefinitions {
+
+    HomePage homepage = new HomePage();
+
+    @Given("go to home page")
+    public void goToHomePage() {
+        Driver.getDriver().get("https://qa.heallifehospital.com/");
+    }
+
+    @Then("the photo confirms that their name and title appear as Professor")
+    public void ThePhotoConfirmsThatTheirNameAndTitleAppearAsProfessor(List<String> isim) {
+
+        int expectedSize = 6;
+        int actualSize = homepage.ourDoctorsResimCount();
+        Assert.assertEquals(expectedSize, actualSize);
+
+        for (int i = 0; i < isim.size(); i++) {
+            Assert.assertEquals(isim.get(i), homepage.ourDoctorsIsim.get(i).getText());
+            Assert.assertEquals("Professor", homepage.ourDoctorsUnvan.get(i).getText());
+        }
+    }
+    @And("user should be able to on the IPD page")
+    public void userShouldBeAbleToOnTheIPDPage() {
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("admin/patient/ipdsearch"));
+    }
 }
